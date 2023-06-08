ffb37ab0f0b5e29ccb3e2bc6fdd8d9bd7a25b0d7
Ricardo Franco Delgado <rf041555@gmail.com>
2023-06-08 13:43:40 -0300

    some adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,90 @@
-import logo from '../public/vite.svg';
-import './App.css';
+import logo from '../public/logo.png'
+import './App.css'
 
 const products = [
   {
     id: 1,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 2,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 3,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 4,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 5,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 6,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 7,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 8,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 9,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 10,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 11,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150'
   },
   {
     id: 12,
     name: 'Produto 1',
     price: '$10',
-    image: 'https://via.placeholder.com/150',
-  },
-
-];
+    image: 'https://via.placeholder.com/150'
+  }
+]
 
 const App = () => {
   return (
     <div className="App">
-      <header className='header-title-logo'>
+      <header className="header-title-logo">
         <img src={logo} alt="Logo" className="logo" />
-        <span>Sublee Outleet</span>
+        <span className="App-title">Outlet</span>
       </header>
       <div className="products-container">
-        {products.map((product) => (
+        {products.map(product => (
           <div className="product" key={product.id}>
             <img
               src={product.image}
@@ -98,7 +97,7 @@ const App = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
